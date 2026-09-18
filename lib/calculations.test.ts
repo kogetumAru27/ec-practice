66157931122e3ee8bdc334d03afc746cdbfb01d5
kogetumAru27 <tm10tora@icloud.com>
2026-09-18 11:43:35 +0900

    失敗例

--- lib/calculations.test.ts
+++ lib/calculations.test.ts
@@ -13,6 +13,6 @@ describe("calculateCartTotal", () => {
             },
         ];
         const total = calculateCartTotal(dummyCartItems);
-        expect(total).toBe(5500);
+        expect(total).toBe(9900);
     })
 })
